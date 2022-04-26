849d1c5604755725b2d3deabe6211a0b630472db
Vidhaat Dunna <dunna.vidhaat@gmail.com>
2022-04-26 12:00:54 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+playing around to get familiarity with p5.js, trying to port over a previous python project
+
+additionally, test for seeing if github pages works with this
---
+++ sketch.js
@@ -0,0 +1,77 @@
+w = window.innerWidth;
+h = window.innerHeight;
+
+function setup(){
+    createCanvas(w, h);
+    background(0, 0, 0);
+}
+
+dots = [];
+
+for (i = 0; i < 50; i++){
+    dots.push(
+        {
+            x : Math.random() * w,
+            y : Math.random() * h,
+            dx : Math.random() - 1,
+            dy : Math.random() - 1,
+            iterate : function(){
+                this.x += this.dx * 0.0002;
+                this.y += this.dy * 0.0002;
+                this.x = max(this.x, 0);
+                this.x = min(this.x, w);
+                this.y = max(this.y, 0);
+                this.y = min(this.y, h);
+                this.dx *= 0.5;
+                this.dy *= 0.5;
+            }
+        }
+    )
+}
+
+function getDistance(a, b) {
+    dx = a.x - b.x;
+    dy = a.y - b.y;
+    return(Math.sqrt((dx * dx) + (dy * dy)));
+}
+
+function pline(a, b){
+    stroke(255, 255, 255, (getDistance(a, b)/20));
+    line(a.x, a.y, b.x, b.y);
+}
+
+
+function draw() {
+
+    background(0, 0, 0);
+
+    dots.forEach(e => {
+        dots.forEach(f => {
+
+            if(getDistance(e, f) < 150){
+                e.dx -= (f.x - e.x) * 100;
+                e.dy -= (f.y - e.y) * 100;
+                pline(e, f);
+            }
+            else if(getDistance(e, f) < 500){
+                e.dx += (f.x - e.x);
+                e.dy += (f.y - e.y);
+                pline(e, f);
+            }
+        })
+
+        if(mouseIsPressed === true){
+            if(getDistance({x: mouseX, y: mouseY}, e) < 150){
+                e.dx -= (mouseX - e.x) * 1000;
+                e.dy -= (mouseY - e.y) * 1000;
+            }
+        }
+    });
+
+    dots.forEach(e => {
+        e.iterate();
+        console.log("iterated");
+        stroke(0);
+        ellipse(e.x, e.y, 5, 5);
+    });
+  }
